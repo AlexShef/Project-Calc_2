1c1f2186335067bb756213cbf69fe60ae700427a
Alex <shtefan88@mail.ru>
2015-01-22 18:17:39 +0500

    fix bugs version

--- src/calc/new_project/MainActivity.java
+++ src/calc/new_project/MainActivity.java
@@ -6,29 +6,18 @@ import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
-public class MainActivity extends Activity implements  OnClickListener{
+public class MainActivity extends Activity {
 	
 	final int MENU_RESET_ID = 1;
 	final int MENU_QUIT_ID = 2;
 	
-	Button bt_Add;
-	Button bt_Sub;
-	Button bt_Mul;
-	Button bt_Div;
-	
-	Button bt_Num1;
-	
-	Button bt_Result;
-	
 	TextView tv_Result;
-	EditText et_Display;
+	TextView tv_Display;
 	
 	String s = new String();
 	
@@ -37,7 +26,7 @@ public class MainActivity extends Activity implements  OnClickListener{
 	double b = 0;
 	double result = 0;
 	
-	int Point = 0; 
+	boolean Point = false; 
 	
 	enum Operand {Add,Sub,Mul,Div};
 	Operand Oper = null;
@@ -49,25 +38,12 @@ public class MainActivity extends Activity implements  OnClickListener{
         setContentView(R.layout.main);
         
         // Определяю обьекты по их id
-        bt_Add = (Button) findViewById(R.id.bt_Add);
-        bt_Sub = (Button) findViewById(R.id.bt_Sub);
-        bt_Mul = (Button) findViewById(R.id.bt_Mul);
-        bt_Div = (Button) findViewById(R.id.bt_Div);
-        bt_Result = (Button) findViewById(R.id.bt_Result);
-        
-        bt_Num1 = (Button) findViewById(R.id.bt_Num1);
-        
+             
         tv_Result = (TextView) findViewById(R.id.tv_Result);
-        et_Display = (EditText) findViewById(R.id.et_Display);
+        tv_Display = (TextView) findViewById(R.id.tv_Display);
         
         // Присваиваю обработчик кнопкам
-        bt_Add.setOnClickListener(this);
-        bt_Sub.setOnClickListener(this);
-        bt_Mul.setOnClickListener(this);
-        bt_Div.setOnClickListener(this);
-        
-        bt_Result.setOnClickListener(this);
-        
+               
     }
       
     @Override
@@ -83,8 +59,9 @@ public class MainActivity extends Activity implements  OnClickListener{
     	switch (item.getItemId()) {
     		case MENU_RESET_ID:
     			// очищаем поля
-    			et_Display.setText("0");
+    			tv_Display.setText("0");
     			tv_Result.setText("");
+    			Point = false;
     			break;
     		case MENU_QUIT_ID:
     			// выход из приложения
@@ -94,262 +71,149 @@ public class MainActivity extends Activity implements  OnClickListener{
     	return super.onOptionsItemSelected(item);
 	}
     
-   ////////////////////////////////////////////////////////////////////////////////////////
-    // Button Num0 click
-    public void bt_Num0_Click(View v){    
-    	double value = 0;
-    	
-    	if (TextUtils.isEmpty(et_Display.getText().toString())){
+   ///////////////////////////////////////////////
+    public void Output_Value(String s){
+    /*	if (TextUtils.isEmpty(et_Display.getText().toString())){
     		et_Display.setText("0");
-    	} else {
-    		value = Double.parseDouble(et_Display.getText().toString());
-     		if (value == 0) {
-     			et_Display.setText("0");
-			} else {
-				et_Display.setText(et_Display.getText().toString() + "0");
-			}
-    		
-    	}
+    	} else 
+    	et_Display.setText(et_Display.getText().toString() + s);
+     				Point = true;
+    		value = Double.parseDouble(et_Display.getText().toString());*/
     	
+    	if ((s.equals(".")) && (Point == false)){
+    			tv_Display.setText(tv_Display.getText().toString() + s);
+ 				Point = true;
+     	} else {
+     		if (s.equals(".")){
+     			return;
+     		} else {
+     			if (tv_Display.getText().toString().equals("0")) {
+     				tv_Display.setText(s);
+    			} else {
+    				tv_Display.setText(tv_Display.getText().toString() + s);
+    			}
+     		}
+     	}
+    }
+    //////////////////////////////////////////////   
+    // Button Num0 click
+    public void onNum0Click(View v){    
+    	Output_Value("0");
     } 
     // Button Num1 click
-    public void bt_Num1_Click(View v){    
-    	double value = 0;
-    	
-    	if (TextUtils.isEmpty(et_Display.getText().toString())){
-    		et_Display.setText("1");
-    	} else {
-    		value = Double.parseDouble(et_Display.getText().toString());
-     		if (value == 0) {
-     			et_Display.setText("1");
-			} else {
-				et_Display.setText(et_Display.getText().toString() + "1");
-			}
-    		
-    	}
-    	
+    public void onNum1Click(View v){    
+    	Output_Value("1");
     }
     // Button Num2 click
-    public void bt_Num2_Click(View v){    
-    	double value = 0;
-    	
-    	if (TextUtils.isEmpty(et_Display.getText().toString())){
-    		et_Display.setText("2");
-    	} else {
-    		value = Double.parseDouble(et_Display.getText().toString());
-     		if (value == 0) {
-     			et_Display.setText("2");
-			} else {
-				et_Display.setText(et_Display.getText().toString() + "2");
-			}
-    		
-    	}
+    public void onNum2Click(View v){    
+    	Output_Value("2");
     }
     // Button Num3 click
-    public void bt_Num3_Click(View v){    
-    	double value = 0;
-    	
-    	if (TextUtils.isEmpty(et_Display.getText().toString())){
-    		et_Display.setText("3");
-    	} else {
-    		value = Double.parseDouble(et_Display.getText().toString());
-     		if (value == 0) {
-     			et_Display.setText("3");
-			} else {
-				et_Display.setText(et_Display.getText().toString() + "3");
-			}
-    		
-    	}
+    public void onNum3Click(View v){    
+    	Output_Value("3");   	
     } 
     // Button Num4 click
-    public void bt_Num4_Click(View v){    
-    	double value = 0;
-    	
-    	if (TextUtils.isEmpty(et_Display.getText().toString())){
-    		et_Display.setText("4");
-    	} else {
-    		value = Double.parseDouble(et_Display.getText().toString());
-     		if (value == 0) {
-     			et_Display.setText("4");
-			} else {
-				et_Display.setText(et_Display.getText().toString() + "4");
-			}
-    		
-    	}
+    public void onNum4Click(View v){    
+    	Output_Value("4");    	
     } 
     // Button Num5 click
-    public void bt_Num5_Click(View v){    
-    	double value = 0;
-    	
-    	if (TextUtils.isEmpty(et_Display.getText().toString())){
-    		et_Display.setText("5");
-    	} else {
-    		value = Double.parseDouble(et_Display.getText().toString());
-     		if (value == 0) {
-     			et_Display.setText("5");
-			} else {
-				et_Display.setText(et_Display.getText().toString() + "5");
-			}
-    		
-    	}
+    public void onNum5Click(View v){    
+    	Output_Value("5");   	
     } 
     // Button Num6 click
-    public void bt_Num6_Click(View v){    
-    	double value = 0;
-    	
-    	if (TextUtils.isEmpty(et_Display.getText().toString())){
-    		et_Display.setText("6");
-    	} else {
-    		value = Double.parseDouble(et_Display.getText().toString());
-     		if (value == 0) {
-     			et_Display.setText("6");
-			} else {
-				et_Display.setText(et_Display.getText().toString() + "6");
-			}
-    		
-    	}
-    	
+    public void onNum6Click(View v){    
+    	Output_Value("6");   	
     } 
     // Button Num7 click
-    public void bt_Num7_Click(View v){    
-    	double value = 0;
-    	
-    	if (TextUtils.isEmpty(et_Display.getText().toString())){
-    		et_Display.setText("7");
-    	} else {
-    		value = Double.parseDouble(et_Display.getText().toString());
-     		if (value == 0) {
-     			et_Display.setText("7");
-			} else {
-				et_Display.setText(et_Display.getText().toString() + "7");
-			}
-    		
-    	}
+    public void onNum7Click(View v){    
+    	Output_Value("7");   
     } 
     // Button Num8 click
-    public void bt_Num8_Click(View v){    
-    	double value = 0;
-    	
-    	if (TextUtils.isEmpty(et_Display.getText().toString())){
-    		et_Display.setText("8");
-    	} else {
-    		value = Double.parseDouble(et_Display.getText().toString());
-     		if (value == 0) {
-     			et_Display.setText("8");
-			} else {
-				et_Display.setText(et_Display.getText().toString() + "8");
-			}
-    		
-    	}
-    	
+    public void onNum8Click(View v){    
+    	Output_Value("8");   	
     } 
-    
     // Button Num9 click
-    public void bt_Num9_Click(View v){    
-    	double value = 0;
-    	
-    	if (TextUtils.isEmpty(et_Display.getText().toString())){
-    		et_Display.setText("9");
-    	} else {
-    		value = Double.parseDouble(et_Display.getText().toString());
-     		if (value == 0) {
-     			et_Display.setText("9");
-			} else {
-				et_Display.setText(et_Display.getText().toString() + "9");
-			}
-    		
-    	}
+    public void onNum9Click(View v){    
+    	Output_Value("9");  	
     } 
- // Button Point click
-    public void bt_Point_Click(View v){    
-    	
-    	
-    	if (TextUtils.isEmpty(et_Display.getText().toString())){
-    		et_Display.setText("0.");
-    	} else {
-    		if(Point == 0){
-     			et_Display.setText(".");
-			}     		
-    	}
+    // Button Point click
+    public void onPointClick(View v){    
+    	Output_Value(".");  	
     } 
-    
-    
-    @Override
-    public void onClick(View v){    	
-        
-		// Определяю id нажатой кнопки	
-    	
-    	switch (v.getId()){
-     	// нажата кнопка сложения
-     	case(R.id.bt_Add):
+      
+    // Button Add click
+    public void onAddClick(View v){
      		// проверка на пустоту
-     		if (TextUtils.isEmpty(et_Display.getText().toString())) {
+     		if (TextUtils.isEmpty(tv_Display.getText().toString())) {
    		      return;
    		    }
      		
-     		a = Double.parseDouble(et_Display.getText().toString());
+     		a = Double.parseDouble(tv_Display.getText().toString());
      		
-     		et_Display.setText("0");
+     		tv_Display.setText("0");
      		tv_Result.setText(a + "+");
      		
      		Oper = Operand.Add;
      		
-     		break;
-     	// нажата кнопка вычетание
-     	case(R.id.bt_Sub):
+    }
+    // Button Sub click
+    public void onSubClick(View v){
      		
-     		if (TextUtils.isEmpty(et_Display.getText().toString())) {
+     		if (TextUtils.isEmpty(tv_Display.getText().toString())) {
      		      return;
      		}
      		
-     		a = Double.parseDouble(et_Display.getText().toString());
+     		a = Double.parseDouble(tv_Display.getText().toString());
  			
-     		et_Display.setText("0");
+     		tv_Display.setText("0");
  			tv_Result.setText(a + "-");
  			
  			Oper = Operand.Sub;
     		
-     		break;
-     	// нажата кнопка умножения
-     	case(R.id.bt_Mul):
+    }
+    // Button Sub click
+    public void onMulClick(View v){
      		
-     		if (TextUtils.isEmpty(et_Display.getText().toString())) {
+     		if (TextUtils.isEmpty(tv_Display.getText().toString())) {
      		      return;
   		    }
      		
-     		a = Double.parseDouble(et_Display.getText().toString());
+     		a = Double.parseDouble(tv_Display.getText().toString());
      		
-     		et_Display.setText("0");
+     		tv_Display.setText("0");
      		tv_Result.setText(a + "*");
      		
      		Oper = Operand.Mul;
      		
-     		break;
-     	// нажата кнопка деления
-     	case(R.id.bt_Div):
+    }
+    // Button Sub click
+    public void onDivClick(View v){
      		
-     		if (TextUtils.isEmpty(et_Display.getText().toString())) {
+     		if (TextUtils.isEmpty(tv_Display.getText().toString())) {
      		      return;
      		}
      		
-     		a = Double.parseDouble(et_Display.getText().toString());
+     		a = Double.parseDouble(tv_Display.getText().toString());
  			
-     		et_Display.setText("0");
+     		tv_Display.setText("0");
  			tv_Result.setText(a + "/");
  			
  			Oper = Operand.Div;
     		
-     		break;
-     	case(R.id.bt_Result):
-     		
-     		if (TextUtils.isEmpty(et_Display.getText().toString())) {
+    }		
+    // Button Result click  		
+ 	public void	onResultClick(View v){
+        		
+     		if (TextUtils.isEmpty(tv_Display.getText().toString())) {
      		      return;
      		}
      		
-     		b =  Double.parseDouble(et_Display.getText().toString());
+     		b =  Double.parseDouble(tv_Display.getText().toString());
      	
      	    tv_Result.setText(a + "  " + b);
-     	    // в соответствии со значением переменной Oper
+     	    
+     	    Point = false;
+     	    
      		switch (Oper){
      		case Add:
      			
@@ -378,15 +242,13 @@ public class MainActivity extends Activity implements  OnClickListener{
      			else{
      				Toast.makeText(this, "На ноль делить нельзя",Toast.LENGTH_LONG).show();
      			}
-     				
-     			
      			break;
      			
      		}
-       		et_Display.setText(""+result);
-     		break;
+     		tv_Display.setText(""+result);
      		
-    	}
+     		
     }
-
 }
+
+
